c1c8e5ce03759d480d8322b342170debb9190e21
Elise Phan <aliceconnic@gmail.com>
2019-02-05 23:50:54 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create(first_name: params[:students][:first_name], last_name: params[:students][:last_name])
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to student_path(@student)
   end
 
